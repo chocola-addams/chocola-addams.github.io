fd2f820366d0a5159b8d6801d1c3ff4944b00f60
chocola <chocola_tahiti@hotmail.fr>
2020-06-06 19:22:39 +0200

    json test 4

--- js/album-photo-grid.js
+++ js/album-photo-grid.js
@@ -1,4 +1,4 @@
-function openModal() {
+/*function openModal() {
   document.getElementById("myModal").style.display = "block";
 }
 
@@ -33,12 +33,21 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
   captionText.innerHTML = dots[slideIndex - 1].alt;
-}
+}*/
 
 /* PHOTO GRID TEST */
+/*var img1 = new Image();
+img1.src = "../img/photos/kirby-vanquin/VNK2017-06-28_7888.jpg";
+var img2 = new Image();
+img2.src = "../img/photos/kirby-vanquin/VNK2017-06-28_7875.jpg";
+var img3 = new Image();
+img3.src = "../img/photos/kirby-vanquin/VNK2017-06-28_7890.jpg";
+var img4 = new Image();
+img4.src = "../img/photos/kirby-vanquin/VNK2017-06-28_7921.jpg";
+var photos = [img1, img2, img3, img4];*/
 /*récupération des données de bdd-image.json*/
 var grid = document.querySelector('grid')
-var requestURL = 'https://github.com/chocola-addams/chocola-addams.github.io/tree/master/json/bdd-mage.json';
+var requestURL = 'https://raw.githubusercontent.com/chocola-addams/chocola-addams.github.io/master/json/bdd-image.json';
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
@@ -47,20 +56,28 @@ request.onload = function () {
   var album = request.response;
   showGrid(album);
 };
+
 /*création de la grille*/
 function showGrid(jsonObj) {
   var photos = jsonObj['photos'];
-  var row = document.createElement('div').setAttribute('class','row');
-  var col1=document.createElement('div').setAttribute('class','column');
-  var col2=document.createElement('div').setAttribute('class','columnl');
-  var col3=document.createElement('div').setAttribute('class','column');
-  row.appendChild(col1);
-  row.appendChild(col2);
-  row.appendChild(col3);
-  for (var i = 0; i < photos.length+2; i+3) {
-    row.col1.appendChild(photos[i+0])
-    row.col2.appendChild(photos[i+1])
-    row.col3.appendChild(photos[i+2])
+  var col1 = document.createElement('div');
+  col1.setAttribute('class', 'column');
+  var col2 = document.createElement('div');
+  col2.setAttribute('class', 'column');
+  var col3 = document.createElement('div');
+  col3.setAttribute('class', 'column');
+  document.getElementById("row").appendChild(col1);
+  document.getElementById("row").appendChild(col2);
+  document.getElementById("row").appendChild(col3);
+  for (var i = 0; i < photos.length + 2; i=+3) {
+    var img1 = new Image();
+    img1.src=photos[i].path;
+    var img2 = new Image();
+    img2.src=photos[i+1].path;
+    var img3 = new Image();
+    img3.src=photos[i+2].path;
+    col1.appendChild(img1);
+    col2.appendChild(img2);
+    col3.appendChild(img3);
   };
-  grid.appendChild(row);
 }
